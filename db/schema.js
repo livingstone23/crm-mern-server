e7bf6595ccf24a8b621187b6d970924637ebdfa6
Livingstone <livingstone23@gmail.com>
2020-05-21 09:33:27 -0600

    Actualizo Mutation de pedidos
    Se actualizo los campos requeridos

--- db/schema.js
+++ db/schema.js
@@ -46,6 +46,8 @@ const typeDefs = gql`
     type PedidoGrupo {
         id: ID
         cantidad: Int
+        nombre: String
+        precio: Float
     }
 
     type TopCliente {
@@ -105,6 +107,9 @@ const typeDefs = gql`
     input PedidoProductoInput {
         id: ID
         cantidad: Int
+        nombre: String
+        precio: Float
+
     }
 
     input PedidoInput {
